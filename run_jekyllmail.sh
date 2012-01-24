9ebbd4101662f4d9560b62c45ad522426c0fc489
linpengxuan <linpengxuan@gmail.com>
2012-01-24 09:51:02 -0500

    updated

--- run_jekyllmail.sh
+++ run_jekyllmail.sh
@@ -3,7 +3,10 @@
 # CHANGE ME TO MEET THE NEEDS OF YOUR SERVER CONFIGURATION
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-GEM_PATH=$GEM_PATH:/home/my_username/.gems
-PATH=$PATH:/home/my_username/.gems/bin
-cd /home/my_username/jekyllmail_repo/
+export LANG=en_US.UTF-8
+export GEM_HOME=/usr/local/rvm/gems/ruby-1.9.2-p290
+export GEM_PATH=/usr/local/rvm/gems/ruby-1.9.2-p290:/usr/local/rvm/gems/ruby-1.9.2-p290@global
+export PATH=/usr/local/rvm/gems/ruby-1.9.2-p290/bin:/usr/local/rvm/gems/ruby-1.9.2-p290@global/bin:/usr/local/rvm/rubies/ruby-1.9.2-p290/bin:/usr/local/rvm/bin:$PATH
+
 bundle exec ruby jekyllmail.rb
+
